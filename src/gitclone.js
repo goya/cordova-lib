7bbe21ba67bd616fade20c1cf5f219d06d07d236
Brett Rudd <brettrudd@gmail.com>
2016-11-28 13:38:09 -0800

    CB-11961 do a shallow clone if git_ref is a remote ref

--- src/gitclone.js
+++ src/gitclone.js
@@ -28,28 +28,43 @@ exports.clone = clone;
 
 //  clone_dir, if provided is the directory that git will clone into.
 //  if no clone_dir is supplied, a temp directory will be created and used by git.
-function clone(git_url, git_ref, clone_dir) {
+function clone (git_url, git_ref, clone_dir) {
 
-    var needsGitCheckout = !!git_ref;
+    var needsGitCheckout = false;
+    var cloneArgs = ['clone'];
+    var checkRemoteRef = Q.resolve();
+    var tmp_dir = clone_dir;
+
+    git_ref = git_ref || 'master';
+
     if (!shell.which('git')) {
         return Q.reject(new Error('"git" command line tool is not installed: make sure it is accessible on your PATH.'));
     }
 
     // If no clone_dir is specified, create a tmp dir which git will clone into.
-    var tmp_dir = clone_dir;
     if (!tmp_dir) {
         tmp_dir = path.join(os.tmpdir(), 'git', String((new Date()).valueOf()));
     }
     shell.rm('-rf', tmp_dir);
     shell.mkdir('-p', tmp_dir);
 
-    var cloneArgs = ['clone'];
-    if (!needsGitCheckout) {
-        // only get depth of 1 if there is no branch/commit specified
-        cloneArgs.push('--depth=1');
+    // if git_ref is not 'master' check if it's a branch or tag so we can shallow clone.
+    if (git_ref !== 'master') {
+        var lsRemoteArgs = ['ls-remote', '-ht', '--exit-code', git_url, git_ref];
+        checkRemoteRef = superspawn.spawn('git', lsRemoteArgs);
     }
-    cloneArgs.push(git_url, tmp_dir);
-    return superspawn.spawn('git', cloneArgs)
+
+    return checkRemoteRef
+        .then(function () {
+            cloneArgs.push('--depth=1', '-b', git_ref);
+        })
+        .fail(function () {
+            needsGitCheckout = true;
+        })
+        .then(function () {
+            cloneArgs.push(git_url, tmp_dir);
+            return superspawn.spawn('git', cloneArgs);
+        })
         .then(function () {
             if (needsGitCheckout) {
                 return superspawn.spawn('git', ['checkout', git_ref], {
@@ -57,13 +72,8 @@ function clone(git_url, git_ref, clone_dir) {
                 });
             }
         })
-        .then(function () {
-            return superspawn.spawn('git', ['rev-parse', '--short', 'HEAD'], {
-                cwd: tmp_dir
-            });
-        })
         .then(function (sha) {
-            events.emit('log', 'Repository "' + git_url + '" checked out to git ref "' + (git_ref || 'master') + '" at "' + sha + '".');
+            events.emit('log', 'Repository "' + git_url + '" checked out to git ref "' + git_ref + '" at "' + sha + '".');
             return tmp_dir;
         })
         .fail(function (err) {
